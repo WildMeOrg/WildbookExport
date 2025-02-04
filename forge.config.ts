828684955852681db693d7d36794a6e5ce146183
tsubramanian <tamilselvan@gmail.com>
2025-02-03 20:14:31 -0500

    Update forge.config.ts

--- forge.config.ts
+++ forge.config.ts
@@ -31,6 +31,9 @@ const config: ForgeConfig = {
     new MakerDMG({ name: "WildEx", icon: icon_path + ".icns", overwrite: true }),
     // new MakerZIP({}),
   ],
+    new MakerDeb({ name: "WildEx", icon: icon_path + ".icns", overwrite: true }),
+    // new MakerZIP({}),
+  ],
   plugins: [
     new WebpackPlugin({
       mainConfig,
